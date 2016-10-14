e94a8b32f9241bffdad0e670c26326e40a3ce3d6
Hawkzed <hawkzed@hotmail.co.uk>
2016-10-14 13:17:24 +0100

    Add files via upload

---
+++ Project.1/game2_template/items.py
@@ -0,0 +1,61 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+	
+	"mass": 0.002,
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+	
+	"mass": 2,
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+	
+	"mass": 0.2,
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+	
+	"mass": 0.002,
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+	
+	"mass": 0.5,
+
+    "description": "This student handbook explains everything. Seriously."
+}
